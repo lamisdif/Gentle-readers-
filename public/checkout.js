c68877e974be01d046f79615642a7789a883e485
Lamis Dif <lamisdif@gmail.com>
2025-07-28 23:18:04 +0200

    fix: use relative path for fetch API

--- public/checkout.js
+++ public/checkout.js
@@ -132,7 +132,8 @@ document.getElementById("checkoutForm").addEventListener("submit", async functio
 
   // Send to backend
   try {
-    const response = await fetch("http://127.0.0.1:3000/checkout", {
+    const response = await fetch('/checkout', {
+
       method: "POST",
       headers: {
         "Content-Type": "application/json"
