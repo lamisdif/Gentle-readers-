69cf039d76fa74ff626080836d84b73db093bffd
Lamis Dif <lamisdif@gmail.com>
2025-09-02 00:31:53 +0200

    pleasssssssssssssssssse

--- public/checkout.js
+++ public/checkout.js
@@ -1,5 +1,4 @@
-// JavaScript extracted from checkout.html
-// تعريف Supabase مباشرة
+
 const supabaseUrl = "https://fsximdllrhglabxbqvay.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzeGltZGxscmhnbGFieGJxdmF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTg0NzgsImV4cCI6MjA3MjMzNDQ3OH0.KiRJdFoW4DtDAPMLqH9Im3-37GhIFmD269iDsY7ih2Q"; // حطي هنا المفتاح العام من Supabase
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
